f5736f61d7463a378aca3583b1c4740d22b8e6f1
NattawatPTM <70339908+NattawatPTM@users.noreply.github.com>
2020-09-12 22:16:38 +0700

    Add files via upload

---
+++ TestBlynk.py
@@ -0,0 +1,72 @@
+
+
+
+
+import BlynkLib
+#import ipstw
+import network
+import time
+import machine
+
+import dht
+d = dht.DHT22(machine.Pin(19))
+
+#w=ipstw.IPSTW()
+#w.begin()
+
+WIFI_SSID = "Nattawat"
+WIFI_PASS = "Nattawat0000"
+
+wifi = network.WLAN(network.STA_IF)
+wifi.active(True)
+#wifi.scan()       
+wifi.connect(WIFI_SSID,WIFI_PASS)
+while not wifi.isconnected() :
+  pass
+print(wifi.ifconfig())
+
+BLYNK_AUTH = 'ar6t7d7TNCieS_RnoqIbz_4ubgJ_fJBC'
+# Initialize Blynk
+blynk = BlynkLib.Blynk(BLYNK_AUTH)
+# Start Blynk (this call should never return)
+
+def v3_write_handler(value):
+  print(value)
+ # register the virtual pin
+blynk.add_virtual_pin(3, write=v3_write_handler)
+def on_connect():
+  print("connected")
+  
+blynk.on_connect(on_connect)
+def on_disconnect():
+  print("disconnected")
+blynk.on_disconnect(on_disconnect)
+
+
+# Register virtual pin handler
+def my_user_task():
+  # Read DHT22
+  d.measure() 
+  t=d.temperature() # eg. 23.6 (閹虹煰) 
+  h=d.humidity() # eg. 41.3 (% RH)
+  
+  #w.fill(0)
+  #w.text("Temp=%d " % t,0,0*8)
+  #w.text("Humi=%d " % h,0,(1*8)+1)
+  #w.show()
+  
+  print('Temp={}'.format(t))
+  print('Humi={}'.format(h))
+  
+  blynk.virtual_write(1, t)
+  blynk.virtual_write(2, h)
+
+# Register task handler
+blynk.set_user_task(my_user_task, 2000)
+
+blynk.run()
+
+
+
+
+
---
+++ Wifi-ap-udp-server.py
@@ -0,0 +1,48 @@
+#wifi-ap-udp-server.py
+import network
+ap_if=network.WLAN(network.AP_IF)
+ap_if.active(True)
+
+ap_if.config(essid="EnET-Gr33",password="Nattawat")
+ap_if.config(channel=33,authmode=network.AUTH_WPA2_PSK)
+
+import socket
+import time
+from machine import Pin
+port = 4210
+LEDr = Pin(5,Pin.OUT)
+LEDb = Pin(19,Pin.OUT)
+LEDg = Pin(23,Pin.OUT)
+
+s=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
+ip=ap_if.ifconfig()[0]
+s.bind((ip,port))
+print('waiting...')
+LEDr.value(1)
+
+while True:
+  data,addr=s.recvfrom(1024)
+  print('received:',data,'from',addr)
+  if(data == b'r'):
+    if LEDr.value()==1:
+      LEDr.value(1)
+      LEDb.value(0)
+      LEDg.value(0)
+      
+    elif LEDr.value()==0:
+      LEDg.value(0)
+      LEDb.value(1)
+      time.sleep(1.5)
+      LEDb.value(0) 
+      LEDr.value(1)
+      s.sendto("Turn on LEDr\n",addr)
+        
+  elif(data == b'g'):
+    LEDr.value(0)
+    LEDb.value(0)
+    LEDg.value(1)
+    s.sendto("Turn ON LED\n",addr)
+  else:
+    s.sendto("Unknow command\n",addr)
+
